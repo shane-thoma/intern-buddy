b85798fd5efa8b6c5ecb2c3f76397b4573ae48a8
Noor Diab <nd00034@mix.wvu.edu>
2026-04-07 15:00:32 -0400

    Added more info to home.jsx

--- frontend/src/home.jsx
+++ frontend/src/home.jsx
@@ -6,10 +6,31 @@ function home(){
 
     return(
         <>
-            <section id="internship-section">
+            <h1>internBuddy</h1>
+            <button>Filter</button>
+            <section class="internship-section">
+                <h2>Internships</h2>
+                <div class="internship-box">
+                    <div class="internship-option">
+                        <p class="job-title">IT Internship</p>
+                        <p class="job-company">Microsoft</p>
+                    </div>
+                    <div class="internship-option">
+                        <p class="job-title">Machine Learning Intern</p>
+                        <p class="job-company">ChatGBT</p>
+                    </div>
+                     <div class="internship-option">
+                        <p class="job-title">Backend Developer</p>
+                        <p class="job-company">Google</p>
+                    </div>
+                </div>
 
             </section>
-            <section id="internship-extra-info-section">
+            <section class="internship-position-info-sextion">
+                <div class="internship-position-info-box">
+                    
+                </div>
+
 
             </section>
         </>
