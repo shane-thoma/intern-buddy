9c8c1d4404382721f7539666ec68c11b8e4a7777
Noor Diab <nd00034@mix.wvu.edu>
2026-04-07 19:45:34 -0400

    Created the skeleton for Profile.jsx and UpdateInfo.jsx

--- frontend/src/Profile.jsx
+++ frontend/src/Profile.jsx
@@ -11,13 +11,28 @@ function Profile(){
         <>
             <h1 className="user-greeting">Hi User!</h1>
             <div className="user-info-box">
-
+                <div className="username">User</div>
+                <div className="university">West Virginia University</div>
+                <div className = "major">Major: Computer Science</div>
+                <div className="GPA">GPA: 3.8</div>
+                
                 <div className="update-info-button" onClick={() => setUpdateInfo(!updateInfo)}>
-                    <div className="popup-content">
-                        <UpdateInfo />
-                        <button className = "popup-content-close" onClick = {()=> setUpdateInfo(false)}>Close</button>
+                    <div className="popup-update-info">
+                    {updateInfo &&(
+                        <div className="popup-content">
+                            <UpdateInfo />
+                            <button className = "popup-content-close" onClick = {()=> setUpdateInfo(false)}>Close</button>
+                        </div>
+                    )}
                     </div>
                 </div>
+
+
+            </div>
+
+            <div className="saved-internship-container">
+                
+
             </div>
             <div className="bottom-nav">
                     <Link to="/">Logout</Link>
