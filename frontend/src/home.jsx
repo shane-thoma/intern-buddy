60ef71c4528d3f2ad0117c94885decb37a99b3cd
Noor Diab <nd00034@mix.wvu.edu>
2026-04-07 14:39:03 -0400

    Updated home.jsx

---
+++ frontend/src/home.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+function home(){
+
+    return(
+        <>
+            <section id="internship-section">
+
+            </section>
+            <section id="internship-extra-info-section">
+
+            </section>
+        </>
+    )
+}
+
+export default App
