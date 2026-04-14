2d0f764f4e6bdfb69505554087d6b454e9aeee37
bhavana-pixel <bhavana0412@hotmail.com>
2026-04-14 12:07:47 -0400

    Profile centering.

--- frontend/src/Profile.jsx
+++ frontend/src/Profile.jsx
@@ -1,43 +1,42 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import UpdateInfo from './UpdateInfo'
 import './Home.css';
 import './Profile.css';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-function Profile(){
+function Profile() {
 
     const [updateInfo, setUpdateInfo] = useState(false);
 
-    return(
+    return (
         <>
+        <div className='profile-container'>
             <h1 className="user-greeting">Hi User!</h1>
             <div className="user-info-box">
                 <div className="username">User</div>
                 <div className="university">West Virginia University</div>
-                <div className = "major">Major: Computer Science</div>
+                <div className="major">Major: Computer Science</div>
                 <div className="GPA">GPA: 3.8</div>
-                
+
                 <button className="update-info-button" onClick={() => setUpdateInfo(!updateInfo)}>Update Info</button>
                 {updateInfo && (
                     <div className="popup-content">
-                            <UpdateInfo />
-                            <button className = "popup-content-close" onClick = {()=> setUpdateInfo(false)}>Close</button>
+                        <UpdateInfo />
+                        <button className="popup-content-close" onClick={() => setUpdateInfo(false)}>Close</button>
                     </div>
-
                 )}
             </div>
 
             <div className="saved-internship-container">
-                
 
             </div>
             <div className="bottom-nav">
-                                <Link to="/Login" className="bottom-nav-items">Logout</Link>
-                                <Link to="/Home" className="bottom-nav-items">Home</Link>
-                                <Link to="/Profile" className="bottom-nav-items">Profile</Link>
+                <Link to="/Login" className="bottom-nav-items">Logout</Link>
+                <Link to="/Home" className="bottom-nav-items">Home</Link>
+                <Link to="/Profile" className="bottom-nav-items">Profile</Link>
             </div>
+        </div>
         </>
     )
-
 }
 export default Profile;
