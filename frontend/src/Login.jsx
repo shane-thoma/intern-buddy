e759b902cf76eeacb1d9965ee7abafed934ac90d
bhavana-pixel <bhavana0412@hotmail.com>
2026-04-14 11:49:51 -0400

    Login page fixing.

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,41 +1,42 @@
 // Import Statements
-import {useState} from "react";
-import {useNavigate} from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Login.css";
 
-function Login(){
+function Login() {
 
-    const[username, setUsername]= useState("")
+    const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
-    
+
     const navigate = useNavigate()
 
-    const handleLogin = () =>{
+    const handleLogin = () => {
         navigate('/Home')
     }
 
-    return(
+    return (
         <div className="login-form-container">
-                <form className="login-form">
-                    <h1 className = "login-title"> Login </h1>
-                        <label className="login-form-label">Username:
-                            <input 
-                                type="text"
-                                value = {username}
-                                onChange ={(e) => setUsername(e.target.value)}
-                            ></input>
-                        </label>
-                        <br></br>
-                        <label className="login-form-label">Password:
-                            <input 
-                            type="text"
-                            value = {password}
-                            onChange = {(e) => setPassword(e.target.value)}
-                            ></input>
-                        </label>
-                    <br></br>
-                </form>
-            <button className="login-button" onClick={handleLogin}>Login</button>
+            <form className="login-form">
+                <h1 className="login-title"> Login </h1>
+                <label className="login-form-label">Username:
+                    <input
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    ></input>
+                </label>
+                <br></br>
+                <label className="login-form-label">Password:
+                    <input
+                        type="text"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    ></input>
+                </label>
+                <br></br>
+                <br></br>
+                <button className="login-button" onClick={handleLogin}>Login</button>
+            </form>
         </div>
     )
 }
