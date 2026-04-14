e0cef950b8f5dca56dfdb8cfd19b055d7b92c563
bhavana-pixel <bhavana0412@hotmail.com>
2026-04-14 08:41:34 -0400

    Signup styling.

--- frontend/src/SignUp.jsx
+++ frontend/src/SignUp.jsx
@@ -19,33 +19,34 @@ function SignUp()
     return(
         <div className="signup-form-container">
             <form className = "signup-form">
-                <label className="signup-form-label">Username:
-                    <input
-                        type="text"
-                        value = {username}
-                        onChange = {(e) => setUsername(e.target.value)}
-                    ></input>
+                <h1 className = "signup-title"> Sign Up </h1>
+                    <label className="signup-form-label">Username:
+                        <input
+                            type="text"
+                            value = {username}
+                            onChange = {(e) => setUsername(e.target.value)}
+                        ></input>
+                        <br></br>
+                    </label>
+                    <label className="signup-form-label">Email Address:
+                        <input
+                            type="text"
+                            value = {email}
+                            onChange = {(e) => setEmail(e.target.value)}
+                        ></input>
+                        <br></br>
+                    </label>
+                    <label className="signup-form-label">Password:
+                        <input
+                            type="text"
+                            value = {password}
+                            onChange = {(e) => setPassword(e.target.value)}
+                        ></input>
+                        <br></br>
+                    </label>
                     <br></br>
-                </label>
-                <label className="signup-form-label">Email Address:
-                    <input
-                        type="text"
-                        value = {email}
-                        onChange = {(e) => setEmail(e.target.value)}
-                    ></input>
-                    <br></br>
-                </label>
-                <label className="signup-form-label">Password:
-                    <input
-                        type="text"
-                        value = {password}
-                        onChange = {(e) => setPassword(e.target.value)}
-                    ></input>
-                    <br></br>
-                </label>
-                <br></br>
-                <button className="signUpBtn" onClick={handleSignUp}>Sign Up</button>
-            </form>
+                    <button className="signUpBtn" onClick={handleSignUp}>Sign Up</button>
+                </form>
         </div>
 
     )
