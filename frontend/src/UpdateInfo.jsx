401438fba418872753b39ac9211f46b2442b2f15
bhavana-pixel <bhavana0412@hotmail.com>
2026-04-15 10:58:01 -0400

    Fixed profile updating styling.

--- frontend/src/UpdateInfo.jsx
+++ frontend/src/UpdateInfo.jsx
@@ -6,18 +6,23 @@ function UpdateInfo(){
                 <label className = "update-info-label">First Name:
                     <input type="text"></input>
                 </label>
+                <br></br>
                 <label className = "update-info-label">Last Name:
                     <input type="text"></input>
                 </label>
+                <br></br>
                 <label className = "update-info-label">University:
                     <input type="text"></input>
                 </label>
+                <br></br>
                 <label className = "update-info-label">Major:
                     <input type="text"></input>
                 </label>
+                <br></br>
                 <label className = "update-info-label">GPA:
                     <input type="text"></input>
                 </label>
+                <br></br>
                 <label className = "update-info-label">Years of Experience:
                     <input type="text"></input>
                 </label>
