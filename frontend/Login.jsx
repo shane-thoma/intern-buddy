149745154e08cd0220b5bea99be035741b995c40
Conor <spudsbuddy2202@gmail.com>
2026-04-16 16:41:55 -0400

    SignUp/Login/Home CSS

---
+++ frontend/Login.jsx
@@ -0,0 +1,50 @@
+// Import Statements
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
+
+function Login() {
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    const navigate = useNavigate()
+
+    const handleLogin = () => {
+        navigate('/Home')
+
+        localStorage.setItem('username', username)
+    }
+
+    
+
+    return (
+        <div className="login-form-container">
+            <form className="lsgin-form">
+                <h1 className="login-title"> Login </h1>
+                <label className="login-form-label">Username:
+                    <input
+                        type="text"
+                        className="login-input-bg login-input-border"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        
+                    ></input>
+                </label>
+                <br></br>
+                <label className="login-form-label">Password:
+                    <input
+                        type="password"
+                        className="login-input-bg login-input-border"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    ></input>
+                </label>
+                <br></br>
+                <br></br>
+                <button className="login-button" onClick={handleLogin}>Login</button>
+            </form>
+        </div>
+    )
+}
+export default Login;
