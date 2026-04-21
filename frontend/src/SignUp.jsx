7f57b431f91f1b3fa4c96e9f860389a9cb3657f6
Noor Diab <nd00034@mix.wvu.edu>
2026-04-21 16:21:14 -0400

    Added a signup functionality

--- frontend/src/SignUp.jsx
+++ frontend/src/SignUp.jsx
@@ -5,16 +5,73 @@ import "./SignUp.css";
 
 function SignUp()
 {
+    const skills = ["python","javascript", "C","Eclipse","VSCode","HTML","CSS",
+        "flask","react","ChatGPT","Docker","Google Gemini","Github Co-Pilot",
+        "Microsoft Office Suite", "Pandas", "Node.js","SQL","MySQL","GitHub",
+        "teamwork", "communication", "problem solving","Microsoft Azure","AWS Cloud","java"];
+
+
     const [username, setUsername] = useState("")
     const [email, setEmail] = useState("")
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [major, setMajor] = useState("")
+    const [university, setUniversity] = useState("")
+    const [gpa, setGPA] = useState("")
     const [password, setPassword] = useState("")
+    const [skillOptions, setSkillOptions] = useState(skills)
+    const [selectedSkill, setSelectedSkill] = useState("")
+    const [skillsToAdd, setSkillsToAdd] = useState([])
 
 
     const navigate = useNavigate();
 
-    const handleSignUp = () =>{
+    const handleSignUp = async(e) =>{
+
+        e.preventDefault()
+        try{
+
+            const response = await fetch(`http://127.0.0.1:5002/api/account`, 
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        username, 
+                        firstName, 
+                        lastName, 
+                        major, 
+                        university,
+                        gpa,
+                        skillsToAdd
+                    })
+                }
+            )
+            if(!response.ok)
+            {
+                const errorData = await response.json();
+                throw new Error(errorData.error || `Error: ${response.status} ${response.statusText}`);
+            }
+
+
+
+
+             
+        }
+        catch(err)
+        {
+            console.log("Unable to create account.")
+        }
+
         navigate('/Login');
+       
     }
+
+    const handleAddSkill = async(newSkill) =>{
+        setSkillsToAdd(prevSkills =>[...prevSkills, newSkill])
+    }
+
 
     return(
         <div className="signup-form-container">
@@ -44,6 +101,65 @@ function SignUp()
                         ></input>
                         <br></br>
                     </label>
+                    <label className="signup-form-label">First Name:
+                        <input
+                            type="text"
+                            value = {firstName}
+                            onChange = {(e) => setFirstName(e.target.value)}
+                        ></input>
+                        <br></br>
+                    </label>
+                    <label className="signup-form-label">Last Name:
+                        <input
+                            type="text"
+                            value = {lastName}
+                            onChange = {(e) => setLastName(e.target.value)}
+                        ></input>
+                        <br></br>
+                    </label>
+                    <label className="signup-form-label">Major:
+                        <input
+                            type="text"
+                            value = {major}
+                            onChange = {(e) => setMajor(e.target.value)}
+                        ></input>
+                        <br></br>
+                    </label>
+                    <label className="signup-form-label">University:
+                        <input
+                            type="text"
+                            value = {university}
+                            onChange = {(e) => setUniversity(e.target.value)}
+                        ></input>
+                        <br></br>
+                    </label>
+                    <label className="signup-form-label">GPA:
+                        <input
+                            type="text"
+                            value = {gpa}
+                            onChange = {(e) => setGPA(e.target.value)}
+                        ></input>
+                        <br></br>
+                    </label>
+                    <label className = "signup-form-label"> Add a Skill:
+                    <select
+                    value = {selectedSkill}
+                    onChange={(e) => setSelectedSkill(e.target.value)}
+                    >
+                    <option value = ""> Select a skill </option>
+                    {skillOptions.map((skills) => (
+                        <option key = {skills} value = {skills}>
+                            {skills}
+                        </option>
+                    ))}
+                    </select>
+
+                    <button type = "button" className = "add-skill-button" onClick = {() => handleAddSkill(selectedSkill)}>
+                    Add Skill
+                    </button>
+                    </label>
+
+
                     <br></br>
                     <button className="signUpBtn" onClick={handleSignUp}>Sign Up</button>
                 </form>
